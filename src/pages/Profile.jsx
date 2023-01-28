2ec9a1c004c5a01edace586dc36952f714e6e38f
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2023-01-28 12:24:54 -0300

    fix destructure local storage

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,21 +5,24 @@ import Header from '../components/Header';
 export default function Profile() {
   const history = useHistory();
   const drawSearchIcon = false;
+  const storage = localStorage.getItem('user');
+  const { email } = storage !== null && (JSON.parse(storage));
 
   const handleLogout = () => {
     localStorage.removeItem('user');
+    localStorage.removeItem('favoriteRecipes');
+    localStorage.removeItem('doneRecipes');
+    localStorage.removeItem('inProgressRecipes');
     history.push('/');
   };
 
   return (
     <div>
-      <Header title="Profile" hasSearchIcon={ drawSearchIcon } />
       <div>
-        <span
-          data-testid="profile-email"
-        >
-          {JSON.parse(localStorage.getItem('user'))}
-        </span>
+        <Header title="Profile" hasSearchIcon={ drawSearchIcon } />
+      </div>
+      <div>
+        <span data-testid="profile-email">{email}</span>
       </div>
       <div>
         <button
