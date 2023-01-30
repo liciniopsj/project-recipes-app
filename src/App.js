02cf40a0a84df5e31c9be9798bc93b2ebb2e37be
Daniel Ribeiro <daniel.ribeiro012@hotmail.com>
2023-01-30 15:51:49 -0300

    router:fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
-import Meals from './pages/Meals';
 import Meal from './pages/Meal';
-import Drinks from './pages/Drinks';
 import Drink from './pages/Drink';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
@@ -14,8 +12,8 @@ function App() {
   return (
     <Switch>
       <Route exact component={ Login } path="/" />
-      <Route component={ Meals } path="/meals" />
-      <Route component={ Drinks } path="/drinks" />
+      <Route component={ Recipes } path="/meals" />
+      <Route component={ Recipes } path="/drinks" />
       <Route component={ Profile } path="/profile" />
       <Route component={ DoneRecipes } path="/done-recipes" />
       <Route component={ FavoriteRecipes } path="/favorite-recipes" />
