6df8c85cc982a50e9475d829b817ecf2ebdcb229
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2023-01-31 20:39:57 -0300

    Req 24 - 26

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Meals from './pages/Meals';
-import Meal from './pages/Meal';
+import RecipeDetails from './pages/RecipeDetails';
 import Drinks from './pages/Drinks';
-import Drink from './pages/Drink';
 import Profile from './pages/Profile';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
@@ -14,14 +13,14 @@ function App() {
   return (
     <Switch>
       <Route exact component={ Login } path="/" />
-      <Route component={ Meals } path="/meals" />
-      <Route component={ Drinks } path="/drinks" />
+      <Route exact component={ Meals } path="/meals" />
+      <Route exact component={ Drinks } path="/drinks" />
       <Route component={ Profile } path="/profile" />
       <Route component={ DoneRecipes } path="/done-recipes" />
       <Route component={ FavoriteRecipes } path="/favorite-recipes" />
       <Route component={ InProgress } path="/drinks/:id-da-receita/in-progress" />
-      <Route component={ Meal } path="/meals/:id-da-receita" />
-      <Route component={ Drink } path="/drinks/:id-da-receita" />
+      <Route path="/meals/:id" component={ RecipeDetails } />
+      <Route path="/drinks/:id" component={ RecipeDetails } />
     </Switch>
   );
 }
--- src/pages/Meal.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Meal() {
-  return (
-    <h1>Meal</h1>
-  );
-}
